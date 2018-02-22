74197d3e060fc758d6599c29d46a1ea4e71b618c
Mir Ayman Ali <alimirayman@outlook.com>
2018-02-22 22:21:27 +0600

    style changes

--- src/App.vue
+++ src/App.vue
@@ -33,11 +33,11 @@ export default {
     background-image: #fafafa;
     background-blend-mode: overlay;
     background-size: cover;
-    position: absolute;
+    position: fixed;
     top: 0;
     left: 0;
     right: 0;
-    bottom: 4px;
+    bottom: 0;
     z-index: -1;
   }
   .powered-by{
