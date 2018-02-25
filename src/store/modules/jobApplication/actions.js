2986850aee2b70631365dc15b08c378a51b42953
Mir Ayman Ali <alimirayman@outlook.com>
2018-02-25 18:36:13 +0600

    give user proper message

--- src/store/modules/jobApplication/actions.js
+++ src/store/modules/jobApplication/actions.js
@@ -4,16 +4,25 @@ import { Message } from 'element-ui'
 
 const actions = {
   async APPLY ({ getters, commit }) {
-    let data = await axios.post(APPLICANT, getters.applicant)
-    console.log(data)
-    commit('nextStep')
-    Message.success({
-      message: 'Congrats, Your Application has been submitted.'
-    })
-    setTimeout(() => {
-      commit('resetStep')
-      commit('resetApplicant')
-    }, 2000)
+    try {
+      let { data } = await axios.post(APPLICANT, getters.applicant)
+      console.log(data)
+      commit('nextStep')
+      Message.success({
+        message: 'Congrats, Your Application has been submitted. Check your email for confirmation. You might need to check your spam email',
+        duration: 5000,
+        showClose: true
+      })
+      setTimeout(() => {
+        commit('resetStep')
+        commit('resetApplicant')
+      }, 5000)
+    } catch (err) {
+      Message.error({
+        message: 'Error Occured!! Please fill the form properly and submit the form again',
+        showClose: true
+      })
+    }
   },
   SAVE_DATA ({commit}, payload) {
     if (!(payload.name === '' || typeof payload.name === 'undefined') &&
