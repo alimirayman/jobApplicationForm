caa66c2e384c4965e3e50610fb076d559f355f1c
Mir Ayman Ali <alimirayman@outlook.com>
2018-02-27 20:20:47 +0600

    Add basic Login page

---
+++ src/pages/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-card class="box-card text-center">
+    <h1>
+      Login
+    </h1>
+    <el-form ref="form" :model="form" label-width="120px">
+      <el-form-item label="Eamil">
+        <el-input v-model="form.email"></el-input>
+      </el-form-item>
+      <el-form-item label="Password">
+        <el-input type="password" v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="button-full" type="primary" @click="submit">Login</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  data () {
+    return {
+      form: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    ...mapActions({
+      login: 'LOGIN'
+    }),
+    submit () {
+      this.login(this.form)
+    }
+  }
+}
+</script>
